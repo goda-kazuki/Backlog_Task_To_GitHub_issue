366de3cb1a3c8334ccb0a3ed45f8c2f3a8d46b15
合田和樹 <goda@eyemovic.com>
2021-07-12 14:23:25 +0900

    動く所まで

---
+++ sample.js
@@ -0,0 +1,26 @@
+const {Octokit} = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: "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",
+});
+
+createIssue()
+
+// async function issueList() {
+//   const issues = await octokit.rest.issues.listForRepo({
+//     owner: 'goda-kazuki',
+//     repo: 'Backlog_Task_To_GitHub_issue',
+//   });
+//   console.log(issues)
+// }
+
+async function createIssue() {
+  const issues = await octokit.rest.issues.create({
+    owner: 'goda-kazuki',
+    repo: 'Backlog_Task_To_GitHub_issue',
+    title: 'Nodejsから追加しましたー',
+    body: '中身'
+  });
+  console.log(issues)
+}
+
